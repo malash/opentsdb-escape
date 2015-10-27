bac1c82f28d33dfd8f3178a47225b1708390f16f
Malash <i@malash.me>
2015-10-27 19:34:24 +0800

    add ESLint

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,18 @@
 'use strict';
 
-var gulp = require('gulp'),
-    plugins = require('gulp-load-plugins')(),
-    tapColorize = require('tap-colorize');
+var gulp = require('gulp');
+var plugins = require('gulp-load-plugins')();
+var tapColorize = require('tap-colorize');
 
-gulp.task('test', function() {
+gulp.task('lint', function() {
+    var eslint = plugins.eslint;
+    return gulp.src(['*.js', 'bin/*.js', 'lib/*.js', 'test/*.js'])
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
+});
+
+gulp.task('test', ['lint'], function() {
     return gulp.src('test/index.js')
         .pipe(plugins.tape({
             reporter: tapColorize()
